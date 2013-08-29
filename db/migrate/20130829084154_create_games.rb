bd3d81f287730d434c67790c66caa2c5cc046f60
djiternia <dhanjayp@iternia.com>
2013-08-29 14:17:29 +0530

    Game Model added

---
+++ db/migrate/20130829084154_create_games.rb
@@ -0,0 +1,12 @@
+class CreateGames < ActiveRecord::Migration
+  def change
+    create_table :games do |t|
+
+      t.belongs_to :user
+      t.integer :played
+      t.integer :win
+      t.integer :lose
+      t.timestamps
+    end
+  end
+end
