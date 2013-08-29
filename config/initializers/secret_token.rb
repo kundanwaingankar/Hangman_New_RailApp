50e8cf9b10a64272b2cb705e508519a9bd07277f
kundan <kundan@iternia.com>
2013-08-29 11:45:20 +0530

    Create Skeleton structure

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+HangmanNewRailsapp::Application.config.secret_key_base = 'ba3c93320a154178a096f136140156545c7b2f461ebeacacaa19dff5af4b62654978902c913e2d74d3f23fa3e06d616b59086013ace48e0b63518c47291bb27b'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+HangmanNewRailsapp::Application.config.session_store :cookie_store, key: '_Hangman_New_RailsApp_session'
