2879f75086ef650d350858ff697dbdb8d34ba4cc
kundan <kundanw1990@gmail.com>
2013-08-30 11:10:48 +0530

    game rspec is updated

--- spec/models/game_spec.rb
+++ spec/models/game_spec.rb
@@ -7,23 +7,22 @@ describe Game do
 
   context ".playing_first_time"  do                      #user is playing game for first time
     it "should have no_of_times_gamePlayed equal to 0" do
-      expect(game.played).to eq(0)
-      expect(game.win).to eq(0)
-      expect(game.lose).to eq(0)
+      game.played.should == 0
+      game.win.should == 0
+      game.lose.should == 0
     end
   end
 
   context ".already_played_games"  do                     #user already played games
     it "should have no_of_times_gamePlayed equal to 8 & won 5 times" do
-      expect(game.played).to eq(8)
-      expect(game.win).to eq(5)
-      expect(game.lose).to eq(3)
+      game.played.should == 8
+      game.win.should == 5
+      game.lose.should == 3
     end
     it "should have no_of_times_gamePlayed equal to 10 & lose 4 times" do
-      expect(game.played).to eq(10)
-      expect(game.win).to eq(6)
-      expect(game.lose).to eq(4)
+      game.played.should == 10
+      game.win.should == 6
+      game.lose.should == 4
     end
   end
 end
-
