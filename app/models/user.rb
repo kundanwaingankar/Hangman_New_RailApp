8d212acfa31cec9a6375f55d546b7a56139c0213
kundan <kundan@iternia.com>
2013-08-30 12:43:39 +0530

    added validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ActiveRecord::Base
+  validates :username, :presence => true ,:message => 'is required' ,:uniqueness => true
+  validates :password, :presence => true ,:message => 'is required'
   has_one :profile
   has_one :game
 end
