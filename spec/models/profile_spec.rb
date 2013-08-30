12700d6384af78b130a0e13c9e1c8c5c6f05049d
prajaktaa <prajaktaa.dabde@gmail.com>
2013-08-30 14:41:11 +0530

    profile rspec is updated

--- spec/models/profile_spec.rb
+++ spec/models/profile_spec.rb
@@ -11,35 +11,25 @@ describe Profile do
   end
 
   it "first_name should not be empty" do
-    #user_profile.firstname = ''
-    user_profile.first_name.should_not == nil
-    #user_profile.firstname = 'abcd'
-    user_profile.first_name.length.should_not == 0
+      user_profile.first_name.should_not == nil
+      user_profile.first_name.length.should_not == 0
   end
 
   it "last_name should not be empty" do
-    #user_profile.lastname = ''
     user_profile.last_name.should_not == nil
-    #user_profile.lastname = 'dfgh'
     user_profile.last_name.length.should_not == 0
   end
 
   it "email should not be empty" do
-    #user_profile.email = ''
     user_profile.email.should_not == nil
-    #user_profile.email = 'abc@gmail.com'
     user_profile.email.length.should_not == 0
-
   end
 
   it "gender should not be empty" do
-    #user_profile.gender = ''
     user_profile.first_name.should_not == nil
-    #user_profile.gender = 'f'
   end
 
   it "email should be valid" do
-    #user_profile.email='abcd@com'
     user_profile.email.should =~ /\A[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]+\z/
   end
 
