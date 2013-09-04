f5e637d79e6201989d33881ef89668d51cb8349b
kundan <kundan@iternia.com>
2013-09-04 16:30:09 +0530

    changed range

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
-  validates :user_name, presence: true, uniqueness: {case_sensitive: false}, length: {in: 6..20, too_long: "%{count} characters minimum allowed",too_short: "%{count} characters maximum allowed"}
+  validates :user_name, presence: true, uniqueness: {case_sensitive: false}, length: {in: 6..25, too_long: "%{count} characters minimum allowed",too_short: "%{count} characters maximum allowed"}
   validates :password, presence: true, length: {in: 6..20, too_long: "%{count} characters minimum allowed",too_short: "%{count} characters maximum allowed"},format: {with: /\d/, message: "Password must contain at least one Number"}
   has_one :profile
   has_one :game
