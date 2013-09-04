0e4fa77ee3988d5adbee6c743330c28a2d7bd596
Ravindra <rkn047@gmail.com>
2013-09-04 17:27:04 +0530

    Callback branch

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ActiveRecord::Base
   validates :password, presence: true, length: {in: 6..20, too_long: "%{count} characters minimum allowed",too_short: "%{count} characters maximum allowed"},format: {with: /\d/, message: "Password must contain at least one Number"}
   has_one :profile
   has_one :game
+
   validates_associated :profile
   validates_associated :game
 
