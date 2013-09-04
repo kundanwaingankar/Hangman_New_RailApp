9453b0751fae37b0645a8482b295394ee8aced02
kundan <kundan@iternia.com>
2013-09-04 15:19:13 +0530

    add Validation to User Model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,9 @@
 class User < ActiveRecord::Base
-  validates :user_name, :presence => true ,:uniqueness => true
-  validates :password, :presence => true
+  validates :user_name, presence: true, uniqueness: {case_sensitive: false}, length: {in: 6..20, too_long: "%{count} characters minimum allowed",too_short: "%{count} characters maximum allowed"}
+  validates :password, presence: true, length: {in: 6..20, too_long: "%{count} characters minimum allowed",too_short: "%{count} characters maximum allowed"},format: {with: /\d/, message: "Password must contain at least one Number"}
   has_one :profile
   has_one :game
+  validates_associated :profile
+  validates_associated :game
 
 end
