4a815b80261affcb3b1a9ad43030d8e0952882d1
shweta <shwetasadawarte88@gmail.com>
2013-09-04 15:44:06 +0530

    validation added to profile model

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,7 +1,6 @@
 class Profile < ActiveRecord::Base
   belongs_to :user
-  validates :first_name, presence: true, format: {with: /^[a-zA-Z .]*$/, message: "Letters, space & dot allowed"}
-  validates :last_name, presence: true, format: {with: /^[a-zA-Z .]*$/, message:  "Letters, space & dot allowed"}
+  validates :first_name, :last_name, presence: true, format: {with: /^[a-zA-Z .]*$/, message: "No special characters allowed except space & dot"}
   validates :gender, presence: true
-  validates :email, presence: true, uniqueness: true, format: {with: /\A[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]+\z/, message: "enter valid email address"}
+  validates :email, presence: true, uniqueness: {case_sensitive: false}, format: {with: /\A[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]+\z/, message: "enter valid email address"}
 end
