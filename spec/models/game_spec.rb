aece7d7823a64276ee445d764e5edcd73cbebafe
shweta <shwetasadawarte88@gmail.com>
2013-09-11 13:25:59 +0530

    profile spec updated

--- spec/models/game_spec.rb
+++ spec/models/game_spec.rb
@@ -2,8 +2,7 @@ require 'spec_helper'
 
 describe Game do
 
-  subject(:game) { Game.new }
-
+  #subject(:game) { Game.new }
 
   context "#initialize" do
 
@@ -22,30 +21,97 @@ describe Game do
   end
 
 
-
   context ".playing_first_time"  do                      #user is playing game for first time
-    it "should have no_of_times_gamePlayed equal to 0" do
-      game.played.should == 0
-      game.win.should == 0
-      game.lose.should == 0
+
+    before do
+      @game = Game.new
+      @game.played = 0
+      @game.win = 0
+      @game.lose = 0
     end
+
+    it "should have no_of_times_game Played equal to 0" do
+      @game.played = 0
+      @game.should be_valid
+    end
+
+    it "should have no_of_times_game win equal to 0" do
+      @game.win = 0
+      @game.should be_valid
+    end
+
+    it "should have no_of_times_game lose equal to 0" do
+      @game.lose = 0
+      @game.should be_valid
+    end
+
+    it "should not have no_of_times_game Played counter greater than 0" do
+      @game.played.should_not eql(2)
+    end
+
+    it "should not have no_of_times_game win counter greater than 0" do
+      @game.win.should_not eql(4)
+    end
+
+    it "should not have no_of_times_game lose counter greater than 0" do
+      @game.lose.should_not eql(3)
+    end
+
+
+    before do
+      @game = Game.new
+    end
+
+    it "no_of_times_game Played counter should exist in user model" do
+      @game.played.should == nil
+      @game.should_not be_valid
+    end
+
+    it "no_of_times_game win counter should exist in user model" do
+      @game.win.should == nil
+      @game.should_not be_valid
+    end
+
+    it "no_of_times_game lose counter should exist in user model" do
+      @game.lose.should == nil
+      @game.should_not be_valid
+    end
+
   end
 
   context ".already_played_games"  do                     #user already played games
-    it "should have no_of_times_gamePlayed equal to 8 & won 5 times" do
-      game.played.should == 8
-      game.win.should == 5
-      game.lose.should == 3
-    end
-    it "should have no_of_times_gamePlayed equal to 10 & lose 4 times" do
-      game.played.should == 10
-      game.win.should == 6
-      game.lose.should == 4
+
+    before do
+      @game = Game.new
+      @game.played = 8
+      @game.win = 5
+      @game.lose = 3
     end
 
-  end
-  it "total played game should be equal to win and loss" do
-    game.played.should == (game.win+game.loss)
+    it "total played game should be equal to (win + loss)" do
+      @game.played.should eq(@game.win + @game.lose)
+    end
+
+    it "total played game should not be different than (win + loss)" do
+      @game.played.should_not eql(10)
+    end
+
+    it "lose game should be equal to (total played game - win)" do
+      @game.lose.should eq(@game.played - @game.win)
+    end
+
+    it "lose game should not be different than (total played game - win)" do
+      @game.lose.should_not eql(0)
+    end
+
+    it "win game should be equal to (total played game - lose)" do
+      @game.win.should eq(@game.played - @game.lose)
+    end
+
+    it "win game should not be different than (total played game - lose)" do
+      @game.win.should_not eql(7)
+    end
+
   end
 
 
