c6147b579715f9e1cdea4b2a7d8a832b6a218b98
Ravindra <rkn047@gmail.com>
2013-09-13 17:24:15 +0530

    Profile added to User

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,9 +3,11 @@ class User < ActiveRecord::Base
   validates :user_name, presence: true, uniqueness: {case_sensitive: false}, length: {in: 6..25, too_long: "%{count} characters minimum allowed",too_short: "%{count} characters maximum allowed"}
   validates :password, presence: true, length: {in: 6..25, too_long: "%{count} characters minimum allowed",too_short: "%{count} characters maximum allowed"},format: {with: /\d/, message: "Password must contain at least one Number"}
   has_one :profile , dependent: :destroy#, :class_name => "Profile"
+  accepts_nested_attributes_for :profile
+
   has_one :game    , dependent: :destroy
 
-  accepts_nested_attributes_for :profile
+
 
   validates_associated :profile
   validates_associated :game
