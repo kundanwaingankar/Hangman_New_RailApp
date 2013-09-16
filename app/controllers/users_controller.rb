283796e8e74e7ea6047717c9716f15755ca2c33c
shweta <shwetasadawarte88@gmail.com>
2013-09-16 18:35:59 +0530

    nested_attributes updated

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,24 +1,32 @@
 class UsersController < ApplicationController
-  #skip_before_filter :verify_authenticity_token
+  skip_before_filter :verify_authenticity_token
   before_action :set_user, only: [:show, :edit, :update, :destroy]
   def index
     @users = User.all
   end
 
   def show
-    @user = User.find(params[:id])
+    @users = User.find(params[:id])
   end
 
   def new
-    @users = User.new
+    #@users = User.new
+    #@users.build_profile
   end
 
   def create
     @users = User.new
     @users.user_name = params[:username]
     @users.password = params[:password]
+    @users.build_profile
+    @users.profile.first_name = params[:first_name]
+    @users.profile.last_name = params[:last_name]
+    @users.profile.gender = params[:gender]
+    @users.profile.email = params[:email]
+
     if @users.save
       redirect_to :action => 'index'
+      flash[:notice] = "Successfully saved!"
     else
       render :action => 'new'
     end
@@ -26,15 +34,16 @@ class UsersController < ApplicationController
 
   def edit
     @users = User.find(params[:id])
+    puts @users.profile.first_name
   end
 
   def update
     @users = User.find(params[:id])
     if @users.update_attributes(user_params)
-      redirect_to :action => 'show', :id => @users, alert: 'Successfully updated...'
+      redirect_to :action => 'show', :id => @users
+      flash[:notice] = "Successfully updated..."
     else
       render :action => 'edit'
-      #flash[:notice] = "Successfully saved!"
     end
   end
 
@@ -45,7 +54,7 @@ class UsersController < ApplicationController
 
   private
   def set_user
-    @user = User.find(params[:id])
+    @users = User.find(params[:id])
   end
 
   private
