0d428fe3eae84810be82861459877d96a50939f3
prajaktaa <prajaktaa.dabde@gmail.com>
2013-09-16 13:10:33 +0530

    user model updated

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,8 @@ class User < ActiveRecord::Base
   validates :password, presence: true, length: {in: 6..25, too_long: "%{count} characters minimum allowed",too_short: "%{count} characters maximum allowed"},format: {with: /\d/, message: "Password must contain at least one Number"}
   has_one :profile , dependent: :destroy, :class_name => "Profile"
   has_one :game   , dependent: :destroy
-  #validates_associated :profile
+  validates_associated :profile
   #validates_associated :game
+  accepts_nested_attributes_for :profile
 
 end
