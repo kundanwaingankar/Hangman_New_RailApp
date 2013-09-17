4a29065bfb867937a998d84019932b7ccb148a2f
Ravindra <rkn047@gmail.com>
2013-09-17 12:30:39 +0530

    Bootstrap Added

--- Gemfile
+++ Gemfile
@@ -7,6 +7,7 @@ gem 'rails', '4.0.0'
 gem 'mysql'
 gem 'haml'
 #gem 'haml-rails','0.3.4'
+gem 'bootstrap-sass'
 
 # Use SCSS for stylesheets
 gem 'sass-rails', '~> 4.0.0'
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130829072459) do
+ActiveRecord::Schema.define(version: 20130829085458) do
+
+  create_table "games", force: true do |t|
+    t.integer  "user_id"
+    t.integer  "played"
+    t.integer  "win"
+    t.integer  "lose"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "plays", force: true do |t|
+    t.integer  "missedCount"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "profiles", force: true do |t|
     t.integer  "user_id"
@@ -24,7 +39,7 @@ ActiveRecord::Schema.define(version: 20130829072459) do
   end
 
   create_table "users", force: true do |t|
-    t.string   "username"
+    t.string   "user_name"
     t.string   "password"
     t.datetime "created_at"
     t.datetime "updated_at"
